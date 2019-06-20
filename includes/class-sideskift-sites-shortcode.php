51cb1a8b55c18ecdcc24c723e5cb4f507f577cea
sideskift <support@sideskift.com>
2019-06-20 11:32:59 +0200

    Indledende commit

---
+++ includes/class-sideskift-sites-shortcode.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Register all actions and filters for the plugin
+ *
+ * @link       https://sideskift.dk
+ * @since      1.0.0
+ *
+ * @package    Sideskift_Sites
+ * @subpackage Sideskift_Sites/includes
+ */
+
+/**
+ * Registers the sideskift shortcode.
+ *
+ * @package    Sideskift_Sites
+ * @subpackage Sideskift_Sites/includes
+ * @author     Henrik Gregersen <henrik@sideskift.dk>
+ */
+
+class Sideskift_Sites_Shortcode {
+
+
+ }
